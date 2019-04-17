396a7d0e71151dceb776e26e0d3e9a24a9964fa1
Rafal Kupka <324064+kupson@users.noreply.github.com>
2019-04-17 17:00:06 +0100

    Handle pagination in Tags()

--- registry/tags.go
+++ registry/tags.go
@@ -1,17 +1,35 @@
 package registry
 
+import (
+	"net/url"
+
+	"github.com/peterhellberg/link"
+)
+
 type tagsResponse struct {
 	Tags []string `json:"tags"`
 }
 
 // Tags returns the tags for a specific repository.
 func (r *Registry) Tags(repository string) ([]string, error) {
-	url := r.url("/v2/%s/tags/list", repository)
-	r.Logf("registry.tags url=%s repository=%s", url, repository)
+	uri := r.url("/v2/%s/tags/list", repository)
+	r.Logf("registry.tags url=%s repository=%s", uri, repository)
 
 	var response tagsResponse
-	if _, err := r.getJSON(url, &response); err != nil {
+	h, err := r.getJSON(uri, &response)
+	if err != nil {
 		return nil, err
+	}
+
+	for _, l := range link.ParseHeader(h) {
+		if l.Rel == "next" {
+			unescaped, _ := url.QueryUnescape(l.URI)
+			tags, err := r.Tags(unescaped)
+			if err != nil {
+				return nil, err
+			}
+			response.Tags = append(response.Tags, tags...)
+		}
 	}
 
 	return response.Tags, nil
